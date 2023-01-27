77fe329855395609c4e9f1ef9a21b14f8ebac899
Shawn Inder <shawninder@gmail.com>
2023-01-26 22:25:35 -0500

    Auto-refresh access tokens

--- src/utils/getAccessTokenForProvider.ts
+++ src/utils/getAccessTokenForProvider.ts
@@ -1,13 +1,34 @@
-import type { Prisma, PrismaClient } from "@prisma/client"
+import type { Account, Prisma, PrismaClient } from "@prisma/client"
 import { type Session } from "next-auth";
 
-type Ctx = {
+export type Ctx = {
   prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>;
   session: Session | null;
+};
+
+export type KnownProvider = 'google' | 'facebook';
+
+export type RefreshedTokens = {
+  ok: boolean
+  access_token: string
+  expires_in: number
+}
+
+export type RefreshError = {
+  error: string
+}
+
+const refreshAccessTokenForProvider = {
+  google: refreshGoogleAccessToken,
+  facebook: refreshFacebookAccessToken,
 }
 
 // Returns a getter function for fetching a provider-specific access token from the database
-export default function getAccessTokenForProvider (provider: string) {
+export default function getAccessTokenForProvider (provider: KnownProvider) {
+  const refreshAccessToken = refreshAccessTokenForProvider[provider];
+  if (!refreshAccessToken) {
+    throw new Error("Missing access token refresher for this provider")
+  }
   // This is the getter function
   return async function getAccessToken (ctx: Ctx): Promise<string> {
     const dbAccount = await ctx.prisma.account.findFirst({
@@ -16,6 +37,107 @@ export default function getAccessTokenForProvider (provider: string) {
         provider // and provider
       }
     });
-    return dbAccount?.access_token || ''; // Return the access token for this user–provider pair, if found
+
+    if (!dbAccount) {
+      throw new Error("Cannot get access token, account not found for this provider")
+    }
+
+    const accessToken = dbAccount.access_token
+    const expiresAt = dbAccount.expires_at
+
+    if (!accessToken || !expiresAt) {
+      throw new Error("Cannot get access token, account doesn't have one…")
+    }
+
+    if (expiresAt < Date.now() / 1000) {
+      // token expired!
+      return refreshAccessToken(ctx, dbAccount);
+    }
+
+    return accessToken; // Return the access token for this user–provider pair, if found
+  }
+}
+
+export async function refreshGoogleAccessToken (ctx: Ctx, dbAccount: Account): Promise<string> {
+  try {
+    const url =
+      `https://oauth2.googleapis.com/token?${(new URLSearchParams({
+        client_id: process.env.GOOGLE_CLIENT_ID || '',
+        client_secret: process.env.GOOGLE_CLIENT_SECRET || '',
+        grant_type: 'refresh_token',
+        refresh_token: dbAccount.access_token || ''
+      })).toString()}`
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    })
+
+    const responseData = await response.json() as RefreshedTokens | RefreshError;
+
+    if (!response.ok) {
+      throw responseData
+    }
+
+    const data = responseData as RefreshedTokens;
+
+    await ctx.prisma.account.update({
+      where: {
+        id: dbAccount.id
+      },
+      data: {
+        access_token: data.access_token,
+        expires_at: Math.round((Date.now() / 1000) + data.expires_in)
+      }
+    })
+
+    return data.access_token;
+  } catch (error) {
+    console.error(error);
+    throw new Error("RefreshGoogleAccessTokenError");
+  }
+}
+
+export async function refreshFacebookAccessToken (ctx: Ctx, dbAccount: Account): Promise<string> {
+  try {
+    const url =
+      `https://graph.facebook.com/v15.0/oauth/access_token?${(new URLSearchParams({
+        grant_type: 'fb_exchange_token',
+        client_id: process.env.FACEBOOK_CLIENT_ID || '',
+        client_secret: process.env.FACEBOOK_CLIENT_SECRET || '',
+        fb_exchange_token: dbAccount.access_token || '',
+      })).toString()}`
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+    })
+
+    const responseData = await response.json() as RefreshedTokens | RefreshError;
+
+    if (!response.ok) {
+      throw responseData
+    }
+
+    const data = responseData as RefreshedTokens;
+
+    await ctx.prisma.account.update({
+      where: {
+        id: dbAccount.id
+      },
+      data: {
+        access_token: data.access_token,
+        expires_at: Math.round((Date.now() / 1000) + data.expires_in)
+      }
+    })
+
+    return data.access_token;
+  } catch (error) {
+    console.log(error);
+    throw new Error("RefreshGoogleAccessTokenError");
   }
 }
